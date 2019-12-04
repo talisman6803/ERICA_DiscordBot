cc69d181244d97405178a475ca6bef4a1be29e8f
Etoile Jeon <wjstkdals409@naver.com>
2019-12-04 21:04:44 +0900

    vote function added.
    vote through message reaction emoji

--- bot_main.py
+++ bot_main.py
@@ -4,12 +4,15 @@ import time
 import asyncio
 import os
 import datetime
+import random
+import re
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 client = discord.Client()
 sched = AsyncIOScheduler()
 sched.start()
 msg_ch = None
+
 
 @client.event
 async def on_ready():
@@ -35,7 +38,15 @@ async def on_message(message):
         text = str(message.content[27:])
         sch(y, m, d, hr, mn, text, message)
         print("alarm job reserved, " + y + m + d + hr + mn + " for " + text)
-
+    
+    elif message.content.startswith('#vote'):
+        await ex(message)
+    
+    elif message.content.startswith('#echo'):
+        target = message.content[6:]
+        await message.channel.send(message.content)
+        print(target.id)
+    
 async def job1(msg, message):
     await message.channel.send('@here' + msg)
     print("alarm job processing")
@@ -44,4 +55,30 @@ def sch(y, m, d, hr, mn, text, message):
     sched.add_job(job1, 'cron', year = y, month = m, day = d, hour = hr, minute = mn, args = [text, message])
     print('launch!')
 
-client.run('NjQyOTI2OTA3MjA5NTQ3Nzkx.Xc1XYA.JSF6rC9gEKgcI5afy1rdjMNNNww')
+async def ex(message):
+
+    choices = {"✅": "Yes",
+               "❎": "No"}
+
+    vote = discord.Embed(title="**[찬반투표]**", description=" ", color=0xff00ff)
+    value = "\n".join("- {} {}".format(*item) for item in choices.items()) 
+    vote.add_field(name="메세지 하단의 이모지를 통해 찬반의사를 표시해주세요(기명)", value=value, inline=True)
+
+    voteargs = await message.channel.send(embed=vote)
+
+    k_lst = choices.keys()
+
+    for key in k_lst:
+        await voteargs.add_reaction(key)
+    await asyncio.sleep(10)
+    msg_id = voteargs.id
+    voteargs = await message.channel.fetch_message(msg_id)
+
+    print(voteargs.reactions[0].count)
+    
+    if(voteargs.reactions[0].count>voteargs.reactions[1].count):
+        await message.channel.send(str(voteargs.reactions[0].count - 1) + " votes in favor(:white_check_mark:).")
+    else:
+        await message.channel.send(str(voteargs.reactions[1].count - 1) + " votes in against(:negative_squared_cross_mark:).")
+    
+
